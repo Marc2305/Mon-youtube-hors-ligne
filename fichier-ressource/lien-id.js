90df878ba9b6749bf4424c8bb8c4948c3217b4f1
Marc <marc.candessoussenswoc@gmail.com>
2020-09-17 22:42:20 +0200

    Nouveau commit

--- fichier-ressource/lien-id.js
+++ fichier-ressource/lien-id.js
@@ -1,3 +1,40 @@
-if (sourceId = "1"){
-    TitreVideo.innerHTML = "Texte";
-}
+//Test lien alert : 
+    if (sourceId = "1"){
+        idVideo.src = "photo-audio-video/video/....mp4";
+        TitreVideo.innerHTML = "Titre 1";
+    }
+    else if (sourceId = "2"){
+        idVideo.src = "";
+        TitreVideo.innerHTML = "";
+    }
+    else if (sourceId = "3"){
+        idVideo.src = "";
+        TitreVideo.innerHTML = "";
+    }
+    else if (sourceId = "4"){
+        idVideo.src = "";
+        TitreVideo.innerHTML = "";
+    }
+    else if (sourceId = "5"){
+        idVideo.src = "";
+        TitreVideo.innerHTML = "";
+    }
+    else if (sourceId = "6"){
+        idVideo.src = "";
+        TitreVideo.innerHTML = "";
+    }
+    else if (sourceId = "7"){
+        idVideo.src = "";
+        TitreVideo.innerHTML = "";
+    }
+    else if (sourceId = "8"){
+        idVideo.src = "";
+        TitreVideo.innerHTML = "";
+    }
+    else if (sourceId = "9"){
+        idVideo.src = "";
+        TitreVideo.innerHTML = "";
+    }
+    else {
+        alert("alert-erreur-code-lien-id");
+    }
